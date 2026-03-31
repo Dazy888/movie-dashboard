85edf1ea1a22733dc20d742f4e70beaf032f2e4b
Давид <8205414@gmail.com>
2026-03-31 20:37:48 +0300

    added VITE_BFF_URL env

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,7 +6,7 @@ import {ApolloProvider} from "@apollo/client/react";
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: 'http://localhost:4000/graphql',
+        uri: import.meta.env.VITE_BFF_URL || 'http://localhost:4000/graphql',
     }),
     cache: new InMemoryCache(),
 });
